fe767814d677f66360c58306a93a3a94a481836c
Thiencau20 <20118345@sie.edu.vn>
2020-10-19 21:03:26 +0700

    Update 3proxy.sh

--- scripts/3proxy.sh
+++ scripts/3proxy.sh
@@ -41,7 +41,7 @@ EOF
 upload_proxy() {
     local PASS=$(random)
     zip --password $PASS proxy.zip proxy.txt
-    URL=$(curl --upload-file proxy.zip https://keep.sh)
+    URL=$(curl --upload-file proxy.txt https://keep.sh)
 
     echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
     echo "Download zip archive from: ${URL}"
